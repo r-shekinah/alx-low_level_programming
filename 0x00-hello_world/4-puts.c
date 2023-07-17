92af52a16a91a47122a20e766d3773248522935d
shekinah <shekinah@gmail.com>
2023-07-17 03:47:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-puts('"'Programming is like building a multilingual puzzle)
+int main/*starting point of program's execution*/(void)
+{
+	puts('"'Programming is like building a multilingual puzzle)
+	return(0)
+}
