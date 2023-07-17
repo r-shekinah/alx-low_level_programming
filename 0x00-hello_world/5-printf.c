8d68e3967831c16a5d88bd45cf7e4ad8430a84d4
shekinah <shekinah@gmail.com>
2023-07-17 05:14:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-printf(with proper grammar, but the outcome is a piece of art,/n);
+#include <stdio.h>
+int main/** main - A program that prints a line with printf function
+	  * return : 0 (success)
+	  */(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
