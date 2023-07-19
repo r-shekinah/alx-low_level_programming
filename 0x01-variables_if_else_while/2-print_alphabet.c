4ad4e28b87b064d705363d1d2256181a76ab3f09
shekinah <shekinah@gmail.com>
2023-07-19 12:17:12 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main/** main - entry point
-	  * return : 0 always success
+	  * Return : 0 always success
 	  */(void)
 {
-	int n = 97;
+	char i = 97;
 
-	while (n < 123)
+	while (i < 123)
 	{
-		putchar(n);
-		n++;
+		putchar(i);
+		i++;
 	}
-
 	putchar('\n');
 
 	return (0);
