89630fbb451a4543203c4221e6eb4838afe9fdbf
shekinah <shekinah@gmail.com>
2023-07-24 09:48:03 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void jack_bauer/* prints each minute across 24 hours
+		*/(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(l + 48);
+				}
+				_putchar(k + 48);
+			}
+			_putchar(j + 48);
+			_putchar(':');
+		}
+		_putchar(i + 48);
+	}
+}
