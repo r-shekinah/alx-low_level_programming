a690c9f9e961b284fb84cffcf310d5e6cb4d0869
shekinah <shekinah@gmail.com>
2023-07-24 09:21:08 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int print_last_digit/* prints last digit of a number
+		     * return : 0 success
+		     */(int i)
+{
+	int l;
+
+	l = i % 10;
+
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else if (l > 0)
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
