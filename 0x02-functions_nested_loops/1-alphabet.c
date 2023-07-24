fb2338de2fdc348942211eaca9880c1c60461c0e
shekinah <shekinah@gmail.com>
2023-07-24 05:48:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet/* print_alphabet - prints the alphabet in lowercase
+		    * return : 0 alwaya
+		    */(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
