5ce6ef28fc6d37f09cd497b54ba9909874399b47
shekinah <shekinah@gmail.com>
2023-07-26 05:44:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,14 @@ void print_to_98/*prints natural numbers from n to 98
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 		}
 	}
 	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 		}
 	}
 }
