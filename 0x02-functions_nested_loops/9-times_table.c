79f3ba8533ba02a9d9c3708cceaf11414145dafa
shekinah <shekinah@gmail.com>
2023-07-28 17:18:04 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,11 +3,43 @@
 void times_table/* prints the 9 times table
 		 */(void)
 {
-	int i, j;
+	int a = 0, b = 0;
 
-	for (i = 0; i < 9; i++)
+	while (b <= 9)
 	{
-		_putchar(i + 48);
-		_putchar(',');
-		_putchar(' ');
+		while (a <= 9)
+		{
+			int product = a * b;
 
+			if (product != 0)
+			{
+				_putchar(',');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			if (product < 10)
+			{
+				_putchar(product + 48);
+			}
+			else
+			{
+				_putchar(product / 10 + 48);
+				_putchar(product % 10 + 48);
+			}
+			if (a == 9)
+			{
+				_putchar('\n');
+			}
+			a++;
+		}
+		a = 0;
+		b++;
+	}
+}
