d4ec259049a149abd23df64ac478501d95968593
shekinah <shekinah@gmail.com>
2023-07-29 14:35:35 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,4 +3,17 @@
 void more_numbers/**
 		  * more_numbers - prints numbers 0 to 14 ten times
 		  */(void)
+{
+	int i, j;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j <= 10)
+				_putchar(49);
+			_putchar(i % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}
