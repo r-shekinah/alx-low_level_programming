304fb13f88a6fae5d9647a2b141fb88d9d091714
shekinah <shekinah@gmail.com>
2023-07-29 11:56:50 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void more_numbers/**
+		  * more_numbers - prints numbers 0 to 14 ten times
+		  */(void)
+
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_line/**
+		* print_line - prints line of length n when n is positive
+		*/(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
