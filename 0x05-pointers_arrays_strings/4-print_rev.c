63476bd0e2f035ee2a3adbc45e134b3eb3138361
shekinah <shekinah@gmail.com>
2023-08-09 15:48:58 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,6 +6,8 @@ void print_rev/**
 		*/(char *s)
 {
 	int i = 0;
+	
+	int j;
 
 	while (*s != '\0')
 	{
@@ -14,7 +16,7 @@ void print_rev/**
 	}
 	s--;
 
-	for (i = 0; i > 0; i--)
+	for (j = i; j > 0; j--)
 	{
 		_putchar(*s);
 		s--;
