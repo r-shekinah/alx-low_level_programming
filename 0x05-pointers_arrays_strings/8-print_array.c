f757e57732b101cc769fff36025ebb2afd087bc3
shekinah <shekinah@gmail.com>
2023-08-11 02:49:51 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_array/**
+		  * print_array - prints n elements of an array of integers
+		  * @a - array
+		  * @n - number of elements to be printed
+		  */(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", *a);
+		a++;
+		i++;
+	}
+	printf("\n");
+}
