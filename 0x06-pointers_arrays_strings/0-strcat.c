86f240d93cddd9dd31eed30ac92ee369c170f03b
shekinah <shekinah@gmail.com>
2023-08-26 13:54:39 -0700

    create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+char *_strcat/**
+	       * *_strcat - concatenates two strings
+	       * returns pointer to resulting string
+	       * *dest, *src - strings to be concatenated
+	       */(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		dest++;
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		j++;
+	}
+
+	return (dest);
+}
