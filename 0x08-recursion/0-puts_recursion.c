2e128594cde3ad4c02fdf40d4e2b3b6f6110ac5c
Riziki <shekinahriziki@gmail.com>
2023-08-30 14:56:55 -0700

    debug 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
-/**
- * _puts_recursion - prints a string 
- * *s - string to be printed
- */
-void _puts_recursion(char *s)
+void _puts_recursion/**
+		     * _puts_recursion - prints a string
+		     * @s - input
+		     */(char *s)
 {
 	if (*s != '\0')
 	{
