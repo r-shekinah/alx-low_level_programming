a3a6df1eef2153760ed6613c730a37b583956dac
Riziki <shekinahriziki@gmail.com>
2023-09-25 16:10:22 -0700

    change header guard

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,9 @@
-#ifndef ABS_H
-#define ABS_H
+#ifndef AB_H
+#define AB_H
 #define ABS(x) result
 #if (x < 0)
 #define result ((-1) * x)
 #else
 #define result x
 #endif /*#if (x < 0)*/
-#endif /*#ifndef ABS_H*/
+#endif /*#ifndef AB_H*/
