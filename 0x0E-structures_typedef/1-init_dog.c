2bf79e0cc60ef4bd62ca658c909e51e091cea393
Riziki <shekinahriziki@gmail.com>
2023-09-26 10:22:41 -0700

    create header file main.h and function 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "dog.h"
+/**
+ * init_dog - initializes variable of type struct dog
+ * @d: name of variable
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	(*d).name = *name;
+	(*d).age = age;
+	(*d).owner = *owner;
+}
