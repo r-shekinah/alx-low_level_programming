f9c4e49b6bd6010aaaf519c2e25c3108add52d8b
r-shekinah <shekinahriziki@gmail.com>
2024-01-28 09:45:02 -0800

    handle task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - copies string to new memory space
+ * @str: string to be copied
+ * Return: pointer to new mem space, NULL for empty string or insufficient mem
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int j = strlen(str);
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup = malloc(j * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= j; i++)
+		dup[i] = str[i];
+	return (dup);
+}
