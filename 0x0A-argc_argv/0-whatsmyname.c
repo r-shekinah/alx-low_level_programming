bd60766df249dc6500f52688a876d05b83ad871a
r-shekinah <shekinahriziki@gmail.com>
2024-02-06 17:06:22 -0800

    resolve betty errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+ * main - prints name of function and new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 is success, else non-zero
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
