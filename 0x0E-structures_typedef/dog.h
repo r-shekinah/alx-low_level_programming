3a607c8fc97fd5472ac7ce0ae89dafdc252980fc
r-shekinah <shekinahriziki@gmail.com>
2024-02-16 17:02:14 -0800

    resolve betty errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - initializes a struture with the following data types
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
